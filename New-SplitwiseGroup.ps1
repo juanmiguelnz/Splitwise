0fadea9c215e6599976ff1a0b8a110b477345732
miguel <miguelkarlojuan@gmail.com>
2023-05-04 00:40:58 +1200

    init New-SplitwiseGroup

---
+++ New-SplitwiseGroup.ps1
@@ -0,0 +1,32 @@
+function New-SplitwiseGroup {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Name,
+
+        [string]$Member
+    )
+
+    Initialize-AWSConfig
+    $Key    = Get-SECSecretValue -SecretId arn:aws:secretsmanager:us-east-1:451460415697:secret:Splitwise-CZz2cB | Select-Object -ExpandProperty SecretString
+    $Token  = $Key | ConvertTo-SecureString -AsPlainText -Force
+
+    $Body = @{
+        name            = $Name
+        users__0__email = $Member
+    }
+
+    $JsonBOdy = $Body | ConvertTo-Json
+
+    $Params = @{
+        Method          = "POST"
+        Uri             = "https://secure.splitwise.com/api/v3.0/create_group"
+        Body            = $JsonBOdy
+        ContentType     = "application/json"
+        Authentication  = "Bearer"
+        Token           = $Token
+    }
+
+    $Request = Invoke-RestMethod @Params
+
+    Write-Output $Request.group
+}
