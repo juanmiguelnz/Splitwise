64700c40252fdc1f2111faf84773a5027f1076ca
miguel <miguelkarlojuan@gmail.com>
2023-05-04 00:30:01 +1200

    store api key in aws secrets manager

--- Add-SplitwiseUserToGroup.ps1
+++ Add-SplitwiseUserToGroup.ps1
@@ -7,7 +7,9 @@ function Add-SplitwiseUserToGroup {
         $UserID
     )
 
-    $Token  = $Env:Splitwise | ConvertTo-SecureString -AsPlainText -Force
+    Initialize-AWSConfig
+    $Key    = Get-SECSecretValue -SecretId arn:aws:secretsmanager:us-east-1:451460415697:secret:Splitwise-CZz2cB | Select-Object -ExpandProperty SecretString
+    $Token  = $Key | ConvertTo-SecureString -AsPlainText -Force
 
     $Body = @{
         group_id    = $GroupID
