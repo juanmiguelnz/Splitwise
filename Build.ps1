65d0b434a552b2a2ef65d3a938a8a0034217330b
miguel <miguelkarlojuan@gmail.com>
2023-05-04 22:09:11 +1200

    add module builder

---
+++ Build.ps1
@@ -0,0 +1,7 @@
+$Params = @{
+    SourcePath                  = ./Splitwise.psd1
+    Version                     = "1.0.$($Env:CODEBUILD_BUILD_NUMBER)"
+    UnversionedOutputDirectory  = $true
+}
+
+Build-Module @Params
