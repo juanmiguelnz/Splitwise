47ee91e9e144a85e621b1832e411fde145693032
miguel <miguelkarlojuan@gmail.com>
2023-05-05 16:41:36 +1200

    test upload artififact

--- Build.ps1
+++ Build.ps1
@@ -1,7 +1,14 @@
-$Params = @{
-    SourcePath                  = "Source\Splitwise.psd1"
-    Version                     = "1.0.$($Env:CODEBUILD_BUILD_NUMBER)"
-    UnversionedOutputDirectory  = $true
+if (Test-Path -Path Env:/AWS_EXECUTION_ENV) {
+    $Params = @{
+        SourcePath                  = "Source\Splitwise.psd1"
+        Version                     = "1.0.$($Env:CODEBUILD_BUILD_NUMBER)"
+        UnversionedOutputDirectory  = $true
+    }
+} else {
+    $Params = @{
+        SourcePath                  = "Source\Splitwise.psd1"
+        Version                     = "0.0.1"
+        UnversionedOutputDirectory  = $true
+    }
 }
-
-Build-Module @Params -Passthru -Verbose
+Build-Module Build-Module @Params
