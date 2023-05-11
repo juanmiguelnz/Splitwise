43981293379d9b78e78b9f677c1ba5efd26b98f8
miguel <miguelkarlojuan@gmail.com>
2023-05-11 19:56:30 +1200

    add pester tests

--- Pester.ps1
+++ Pester.ps1
@@ -1,5 +1,5 @@
 $PesterConfig                           = New-PesterConfiguration
-$PesterConfig.TestResult.OutputFormat   = NUnitXml
-$PesterConfig.TestResult.OutputPath     = Tests/Unit.Test.xml
+$PesterConfig.TestResult.OutputFormat   = "NUnitXml"
+$PesterConfig.TestResult.OutputPath     = "Tests/Unit.Test.xml"
 $PesterConfig.TestResult.Enabled        = $True
 Invoke-Pester -Configuration $PesterConfig
