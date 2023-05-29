12653e0c6ec11e1ee261752b58f0bd01d5a5f956
miguel <miguelkarlojuan@gmail.com>
2023-05-29 23:03:31 +1200

    add amex

--- Source/Public/Invoke-SplitwiseExpense.ps1
+++ Source/Public/Invoke-SplitwiseExpense.ps1
@@ -20,35 +20,36 @@ function Invoke-SplitwiseExpense {
 
         $Csv | ForEach-Object {
 
-            $BankObj = if ($CsvProperties -contains "ProcessedDate") {
-                [PSCustomObject]@{
+            $Bank = if ($CsvProperties -contains "ProcessedDate") {
+                ([PSCustomObject]@{
                     Name            = "ANZ"
                     Details         = $_.Details
                     TransactionDate = (Get-Date -Date $_.TransactionDate -Format "o")
-                }
+                })
             } else {
-                [PSCustomObject]@{
+                ([PSCustomObject]@{
                     Name            = "AMEX"
                     Details         = $_.Description
                     TransactionDate = (Get-Date -Date $_.Date -Format "o")
-                }
+                })
             }
 
-            if ($BankObj.Details -like "*payment*") {
-                Write-Output $BankObj.Details
+            if ($Bank.Details -like "*payment*") {
+
+                Write-Output $Bank.Details
 
             } else {
-                if (($BankObj.Details -notlike "Iag*") -and
-                    ($BankObj.Details -notlike "Myrepublic*") -and
-                    ($BankObj.Details -notlike "Countdown*") -and
-                    ($BankObj.Details -notlike "Pak*") -and
-                    ($BankObj.Details -notlike "Skinny*")) {
+                if (($Bank.Details -notlike "Iag*") -and
+                    ($Bank.Details -notlike "Myrepublic*") -and
+                    ($Bank.Details -notlike "Countdown*") -and
+                    ($Bank.Details -notlike "Pak*") -and
+                    ($Bank.Details -notlike "Skinny*")) {
 
                         $Body = @{
                             group_id        = $GroupID
-                            description     = $BankObj.Details
+                            description     = $Bank.Details
                             cost            = $_.Amount
-                            date            = $BankObj.TransactionDate
+                            date            = $Bank.TransactionDate
                             split_equally   = $True
                         }
     
