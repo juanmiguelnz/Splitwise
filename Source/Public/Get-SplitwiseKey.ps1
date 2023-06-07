a2c299df6e481fd9b9638298c654eaaa9d402649
miguel <miguelkarlojuan@gmail.com>
2023-06-07 20:50:27 +1200

    debug

--- Source/Public/Get-SplitwiseKey.ps1
+++ Source/Public/Get-SplitwiseKey.ps1
@@ -1,6 +1,6 @@
 function Get-SplitwiseKey {
 
-    Initialize-AWSConfig
+#    Initialize-AWSConfig
     $Key    = Get-SECSecretValue -SecretId arn:aws:secretsmanager:us-east-1:451460415697:secret:Splitwise-CZz2cB | Select-Object -ExpandProperty SecretString
     $Token  = $Key | ConvertTo-SecureString -AsPlainText -Force
 
