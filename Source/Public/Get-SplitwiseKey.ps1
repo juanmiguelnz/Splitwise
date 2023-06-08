eedd20596983a93281e26021713dd37310f4c57f
Miguel Juan <52803779+juanmiguelnz@users.noreply.github.com>
2023-06-08 12:24:13 +0000

    debug

--- Source/Public/Get-SplitwiseKey.ps1
+++ Source/Public/Get-SplitwiseKey.ps1
@@ -1,9 +1,9 @@
 function Get-SplitwiseKey {
 
     Get-AWSCredential -ListProfileDetail
-    Set-AWSCredential -ProfileName default
-    $Creds  = Initialize-AWSAssumeRole -RoleArn "arn:aws:iam::451460415697:role/Role-SecretManager-ProdAccount-Access" -RoleSessionName "CodeBuildSession"
-    $Key    = Get-SECSecretValue -SecretId "arn:aws:secretsmanager:us-east-1:451460415697:secret:Splitwise-CZz2cB" -Credential $Creds | Select-Object -ExpandProperty SecretString
+
+    $Creds = (Use-STSRole -RoleArn "arn:aws:iam::869073653340:role/service-role/AwsDevOpsCodeBuildServiceRole" -RoleSessionName "HelloWorld").Credentials
+    $Key    = get-secSecretValue -SecretId "arn:aws:secretsmanager:us-east-1:451460415697:secret:Splitwise-CZz2cB" -Credential $Creds | Select-Object -ExpandProperty SecretString
     $Token  = $Key | ConvertTo-SecureString -AsPlainText -Force
 
     Write-Output $Token
