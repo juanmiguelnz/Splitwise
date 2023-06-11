d69cb2589dc4968e8aa2afd85b7462dec16c9314
Miguel Juan <52803779+juanmiguelnz@users.noreply.github.com>
2023-06-11 12:25:50 +0000

    debug

--- Source/Public/Get-SplitwiseKey.ps1
+++ Source/Public/Get-SplitwiseKey.ps1
@@ -1,7 +1,6 @@
 function Get-SplitwiseKey {
 
-    $Key    = Get-SECSecretValue -SecretId "arn:aws:secretsmanager:us-east-1:451460415697:secret:Splitwise-CZz2cB" -Credential (Get-AWSCredential) | Select-Object -ExpandProperty SecretString
-    $Token  = $Key | ConvertTo-SecureString -AsPlainText -Force
+    $Token  = $Env:SPLITWISE_KEY | ConvertTo-SecureString -AsPlainText -Force
 
     Write-Output $Token
 }
